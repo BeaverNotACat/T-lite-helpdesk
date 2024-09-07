7a585c826a6434357e95263904f756d572f62cd1
BeaverNotACat <beavernotacat@gmail.com>
2024-09-07 21:48:59 +1000

    Change 400 status code to 422

--- api/app/app.py
+++ api/app/app.py
@@ -1,4 +1,5 @@
-from litestar import Litestar
+from litestar import Litestar, MediaType, Request, Response
+from litestar.exceptions import HTTPException, ValidationException
 from litestar.di import Provide
 from litestar.config.cors import CORSConfig
 
@@ -7,13 +8,37 @@ from app.dependencies import model_dependencie, tokenizer_dependencie
 from app.routes import Assist
 
 
+def validation_exception_handler(
+    request: Request, exc: ValidationException
+) -> Response:
+    """Make validation error as it was decribed in contract"""
+    return Response(
+        media_type=MediaType.TEXT,
+        content=f"""
+{
+  "detail": [
+    {
+      "loc": [
+        "string",
+        0
+      ],
+      "msg": "{exc.detail}",
+      "type": "{exc}"
+    }
+  ]
+}
+        """,
+        status_code=422,
+    )
+
+
 cors_config = CORSConfig(allow_origins=["*"])
 
 app = Litestar(
     lifespan=[model_setup, tokenizer_setup],
-    dependencies = {
+    dependencies={
         "model": Provide(model_dependencie),
-        "tokenizer": Provide(tokenizer_dependencie)
+        "tokenizer": Provide(tokenizer_dependencie),
     },
     route_handlers=[Assist],
     cors_config=cors_config,
